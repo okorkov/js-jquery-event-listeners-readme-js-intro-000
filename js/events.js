82ab840141024dd1c0baa70696c220548b790af3
Aliaksandr Okarkau <alex.okorkov@gmail.com>
2020-11-03 22:29:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -17,6 +17,6 @@ function submitIt(){
 $(document).ready(function(){
   $ ("p").on('click', getIt);
   $ ('img').on('load', frameIt);
-  $ ('#typing').on('keydowm', pressIt);
+  $ ('#typing').on('keydown', pressIt);
   $ ('form').on("submit", submitIt);
 });
