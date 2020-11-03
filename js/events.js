4039a47a5656bd4e6d9a9a7fe3451a68eff3ddb3
Aliaksandr Okarkau <alex.okorkov@gmail.com>
2020-11-03 22:40:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
 //define functions here
+function getIt(){
+  alert("Hey!")
+}
+function frameIt(){
+  $( 'img' ).addClass( "tasty" );
+}
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      alert("G was pressed.");
+    }
+  });
+}
+function submitIt(){
+  alert("Your form is going to be submitted now.");
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  $ ("p").on('click', getIt);
+  $ ('img').on('load', frameIt);
+  pressIt();
+  $ ('form').on("submit", submitIt);
 });
